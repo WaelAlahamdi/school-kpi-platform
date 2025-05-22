0604b90a01625030d726c37665aad2e6ed29fa13
Wael Alahmadi <s223837765@deakin.edu.au>
2025-05-23 00:00:38 +1000

    Remove deprecated /indicators test

--- kpi_test_app.py
+++ kpi_test_app.py
@@ -3,13 +3,3 @@ def test_home():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"School KPI" in response.data
-
-def test_indicators():
-    from kpi_app import app
-    client = app.test_client()
-    response = client.get('/indicators')
-    assert response.status_code == 200
-    data = response.get_json()
-    assert "schools" in data
-    assert isinstance(data["schools"], list)
